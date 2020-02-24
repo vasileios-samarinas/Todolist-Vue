35a056e6df8ab8082b53a6b477e2a20df055015d
vasileios-samarinas <vasileios.samarinas@gmail.com>
2020-02-24 19:40:25 +0000

    MVP completed-1

--- src/app.js
+++ src/app.js
@@ -13,27 +13,3 @@ document.addEventListener('DOMContentLoaded', () => {
           this.newItem = "";
         }
       }
-
-
-
-
-
-
-    });
-  });
-  //     },
-  //     methods: {
-  //       saveNewItem:function(){
-  //         this.items.push({
-  //           name:this.newItem,
-  //           isPurchased:false
-  //         });
-  //         this.newItem="";
-  //       },
-  //       buyItem: function(index){
-  //         this.items[index].isPurchased =true;
-  //         // this.items[index].isPurchased =!this.items[index].isPurchased
-  //       }
-  //     }
-  //   });
-  // });
